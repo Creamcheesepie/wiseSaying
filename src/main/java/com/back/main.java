9c44588d8db85fc1a8e5a0313c64637a3727afaf
khy <gnldbs1004@naver.com>
2025-08-01 09:35:25 +0900

    git add

---
+++ src/main/java/com/back/main.java
@@ -0,0 +1,10 @@
+package com.back;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("=== 명언 앱 ===");
+        System.out.print("명령 : ");
+    }
+
+    private static void
+}
